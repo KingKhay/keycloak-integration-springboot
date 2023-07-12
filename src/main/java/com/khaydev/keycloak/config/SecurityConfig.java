0548a189358097c5e50f37897ee6bd09286fe1aa
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-12 11:18:58 +0000

    refactor security config

--- src/main/java/com/khaydev/keycloak/config/SecurityConfig.java
+++ src/main/java/com/khaydev/keycloak/config/SecurityConfig.java
@@ -24,7 +24,8 @@ public class SecurityConfig {
     }
 
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http
+                .authorizeHttpRequests()
                 .requestMatchers("/users*")
                 .hasRole("ADMIN")
                 .anyRequest()
