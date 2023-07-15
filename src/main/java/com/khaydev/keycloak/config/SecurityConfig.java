07a6dd54f5a219e5e53437f3961a3c87d3974be7
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-15 19:20:39 +0000

    refactor security config

--- src/main/java/com/khaydev/keycloak/config/SecurityConfig.java
+++ src/main/java/com/khaydev/keycloak/config/SecurityConfig.java
@@ -6,10 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @EnableWebSecurity
@@ -27,8 +25,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests()
-                .requestMatchers("/users*")
-                .hasRole("ADMIN")
+                .requestMatchers("/users/**").hasRole("USER")
+                .requestMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest()
                 .permitAll();
 
