9e2cab943c64ee3d6045c612963d711d48c35374
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-15 19:16:49 +0000

    enabled method level security

--- src/main/java/com/khaydev/keycloak/KeycloakApplication.java
+++ src/main/java/com/khaydev/keycloak/KeycloakApplication.java
@@ -2,8 +2,10 @@ package com.khaydev.keycloak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class KeycloakApplication {
 
 	public static void main(String[] args) {
